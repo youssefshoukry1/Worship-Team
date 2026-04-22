42ca486d8c98fd9eacedeaf9be6917d4d1373932
youssefshoukry1 <youssefshoukry1@hotmail.com>
2026-04-23 00:35:45 +0200

    update project

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,13 @@
-import type { NextConfig } from "next";
-
-// Check if we are on Vercel (Vercel sets this variable automatically)
-const isVercel = process.env.VERCEL === '1';
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-
-  // 1. Always use 'export' for static site generation
-  output: 'export',
-
-  // 2. Only use './' if it's a production build AND NOT on Vercel (e.g., for Electron)
-  // 3. If it's Vercel or Development, use ''
-  assetPrefix: (isProd && !isVercel) ? './' : '',
-
-  trailingSlash: true,
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**', // This allows all images from your Cloudinary account
+      },
+    ],
   },
 };
 
