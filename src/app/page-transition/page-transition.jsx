3124931a494c3f83b4838c67ad2918c04f297893
youssefshoukry1 <youssefshoukry1@hotmail.com>
2026-08-12 00:07:07 +0300

    update page transation

--- src/app/page-transition/page-transition.jsx
+++ src/app/page-transition/page-transition.jsx
@@ -9,23 +9,12 @@ export default function PageTransition({ children }) {
         <AnimatePresence mode="wait">
             <motion.div
                 key={pathname}
-                initial={{ opacity: 0, filter: "blur(15px)" }}
+                initial={{ opacity: 0, filter: "blur(12px)" }}
                 animate={{ opacity: 1, filter: "blur(0px)" }}
-                exit={{ opacity: 0, filter: "blur(15px)" }}
-                transition={{ duration: 0.6, ease: "easeInOut" }}
-                className="relative min-h-screen w-full"
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+                className="min-h-screen w-full"
             >
-                {/* Overlay الأزرق السمواوي */}
-                <motion.div
-                    initial={{ opacity: 1 }}
-                    animate={{ opacity: 0 }}
-                    exit={{ opacity: 1 }}
-                    transition={{ duration: 0.6, ease: "easeInOut" }}
-                    className="absolute inset-0 bg-[#0a3d62]/60 pointer-events-none"
-                />
-
-                {/* الصفحة نفسها */}
-                <div className="relative z-10">{children}</div>
+                {children}
             </motion.div>
         </AnimatePresence>
     );
